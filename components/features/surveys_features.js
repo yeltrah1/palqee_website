b5a1e527b3aa289c3d071a218719f545a3027b0d
andrehqs <andrehqs.dev@hotmail.com>
2020-07-10 01:17:43 +0100

    hooks

--- components/features/surveys_features.js
+++ components/features/surveys_features.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import translate from "../../providers/i18n/translate";
@@ -81,19 +81,118 @@ const Cards = styled.div`
     }
 `;
 
+const Slider = styled.div`
+    transform: translateX(-${props => props.translate}px);
+    transition: transform ease-out ${props => props.transition}s;
+    height: 100%;
+    width: ${props => props.width}px;
+    display: flex;
+`;
+
 const SurveysFeatures = () => {
     
+    const firstCard = features[0]
+    const secondCard = features[1]
+    const lastCard = features[features.length - 1]
+    const width = 260
+
+    const [state, setState] = useState({
+        activeCard: 0,
+        translate: width,
+        transition: 0.45,
+        _slides: [lastCard, firstCard, secondCard]
+    })
+
+    const { activeCard, translate, _slides, transition } = state
+
+    const autoPlayRef = useRef()
+    const transitionRef = useRef()
+
+    const nextSlide = () => setState({
+      ...state,
+      translate: translate + width,
+      activeCard: activeCard === features.length - 1 ? 0 : activeCard + 1
+    })
+
+    const smoothTransition = () => {
+        let _slides = []
+
+        // We're at the last slide.
+        if (activeCard === features.length - 1)
+            _slides = [features[features.length - 2], lastCard, firstCard]
+        // We're back at the first slide. Just reset to how it was on initial render
+        else if (activeCard === 0) _slides = [lastCard, firstCard, secondCard]
+        // Create an array of the previous last slide, and the next two slides that follow it.
+        else _slides = features.slice(activeCard - 1, activeCard + 2)
+        
+        setState({
+            ...state,
+            _slides,
+            transition: 0,
+            translate: width
+        })
+    }
+
+    useEffect(() => {
+        autoPlayRef.current = nextSlide
+        transitionRef.current = smoothTransition
+    })
+
+    useEffect(() => {
+        const play = () => {
+          autoPlayRef.current()
+        }
+
+        const smooth = e => {
+            if (e.target.className.includes('SliderContent')) {
+              transitionRef.current()
+            }
+        }
+
+        const transitionEnd = window.addEventListener('transitionend', smooth)
+      
+        let interval = null
+      
+        if (features.autoPlay) {
+           interval = setInterval(play, features.autoPlay * 1000)
+        }
+
+        return () => {
+            window.removeEventListener('transitionend', transitionEnd)
+      
+            if (features.autoPlay) {
+              clearInterval(interval)
+            }
+          }
+    }, [])
+
+    useEffect(() => {
+        if (transition === 0) setState({ ...state, transition: 0.45 })
+    }, [transition])
+
+    const prevSlide = () =>
+    setState({
+      ...state,
+      translate: 0,
+      activeCard: activeCard === 0 ? features.length - 1 : activeCard - 1
+    })
+
     return (
     <ThemeProvider theme={palqeeTheme}>
         <Wrapper>
             <HeaderText>
                 <div className="text">The tools you need at your fingertips</div>
                 <div className="arrows">
-                    <Arrow src={ArrowLeft}/>
-                    <Arrow src={ArrowRight}/> 
+                    <Arrow src={ArrowLeft} onClick={prevSlide}/>
+                    <Arrow src={ArrowRight} onClick={nextSlide}/> 
                 </div>
             </HeaderText>
             <Cards>
+            <Slider
+                translate={translate}
+                transition={transition}
+                width={width * _slides.length}
+            >
                 {features.map(features =>
                 <FeaturesCard 
                     image={features.image}
@@ -102,6 +201,7 @@ const SurveysFeatures = () => {
                     key={features.id}
                 />
                 )}
+            </Slider>
             </Cards>
         </Wrapper>
     </ThemeProvider>
