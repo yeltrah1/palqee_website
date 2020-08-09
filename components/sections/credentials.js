222bf16ea77182feacb5f727cfd303e0bf50c324
andrehqs <andrehqs.dev@hotmail.com>
2020-08-09 15:53:25 +0100

    certifications

--- components/sections/credentials.js
+++ components/sections/credentials.js
@@ -53,7 +53,7 @@ const Text = styled.div`
 
 const ContainerLogos = styled.div`
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
+    grid-template-columns: repeat(6, 1fr);
     column-gap: 20px;
     grid-template-rows: 1fr;
     width: 100%;
@@ -61,7 +61,7 @@ const ContainerLogos = styled.div`
     align-items: center;
     background-image: url("/static/patterns/bg_line.svg");
     background-repeat: no-repeat;
-    background-size: 650px;
+    background-size: 100% 80%;
     background-position: bottom;
 `;
 
@@ -79,12 +79,12 @@ const Credentials = () => {
                 <div className="small">Palqee automates, organises and combines workforce and vendor surveys based on your business setup.</div>
             </Text>
             <ContainerLogos>
-                <Logos width={80} src={"/static/icons/badges/exin.png"}/>
-                <Logos width={140} src={"/static/icons/badges/anppd.png"}/>
-                <Logos width={80} src={"/static/icons/badges/exin.png"}/>
-                <Logos width={80} src={"/static/icons/badges/exin.png"}/>
-                <Logos width={80} src={"/static/icons/badges/exin.png"}/>
-                <Logos width={80} src={"/static/icons/badges/exin.png"}/>
+                <div></div>
+                <Logos width={80} style={{ paddingTop: "100px" }} src={"/static/icons/badges/exin.png"}/>
+                <Logos width={140} style={{ paddingBottom: "80px", paddingRight: "10px" }} src={"/static/icons/badges/anppd.png"}/>
+                <Logos width={80} style={{ paddingBottom: "120px", paddingRight: "30px" }} src={"/static/icons/badges/iapp_cert.png"}/>
+                <Logos width={90} style={{ paddingBottom: "160px", paddingRight: "100px" }} src={"/static/icons/badges/palqee_privacyops.png"}/>
+                <div></div>
             </ContainerLogos>
           </Wrapper>
       </ThemeProvider>
